3db4a401a723eb9f5d4cc566fe010c7f7d9f4fa7
mhousetree <mhousetree@gmail.com>
2021-04-12 03:51:02 +0900

    :tada: initial commit

---
+++ index.js
@@ -0,0 +1,19 @@
+// https://slack.com/oauth/authorize?client_id=1963486779136.1939901061763&scope=identify&redirect_uri=http://localhost:3000/
+
+const request = require('request')
+const express = require('express');
+const path = require('path')
+const app = express();
+
+const slack_client_id = process.env.SLACK_CLIENT_ID;
+const slack_client_secret = process.env.SLACK_CLIENT_SECRET;
+
+app
+    .use(express.static(path.join(__dirname, 'public')))
+    .set('views', path.join(__dirname, 'views'))
+    .set('view engine', 'pug')
+
+
+app.get('/', (req, res) => {
+    res.render('index', {title: 'gokigen-appeal'})
+})
