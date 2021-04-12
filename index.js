96ae5acbf0ecaa60c5adc974f154a36c5a6bd63c
mhousetree <mhousetree@gmail.com>
2021-04-12 09:49:14 +0900

    :sparkles: v1.0.0

--- index.js
+++ index.js
@@ -1,26 +1,50 @@
-// https://slack.com/oauth/authorize?client_id=1963486779136.1939901061763&scope=identify&redirect_uri=http://localhost:3000/
-
-const request = require('request')
 const express = require('express');
 const path = require('path')
+const set_status_emoji = require('./set_status_emoji.js')
 const app = express();
 const PORT = process.env.PORT || 3000
-
-const slack_client_id = process.env.SLACK_CLIENT_ID;
-const slack_client_secret = process.env.SLACK_CLIENT_SECRET;
 
 app
     .use(express.static(path.join(__dirname, 'public')))
     .set('views', path.join(__dirname, 'views'))
     .set('view engine', 'pug')
 
-
 app
     .get('/', (req, res) => {
-        res.render('index', { title: 'gokigen-appeal' })
+        res.render('index', { title: 'GOKIGEN appeal for Slack' })
     })
     .get('/test', (req, res) => {
         res.render('test', { title: 'test page' })
+    })
+    .get('/GOKIGEN-level5', (req, res) => {
+        const level = 5;
+        const redirect_uri = "https://gokigen-appeal.herokuapp.com/GOKIGEN-level5";
+        set_status_emoji(req, level, redirect_uri);
+        res.render('result', { level: 5 })
+    })
+    .get('/GOKIGEN-level4', (req, res) => {
+        const level = 4;
+        const redirect_uri = "https://gokigen-appeal.herokuapp.com/GOKIGEN-level4";
+        set_status_emoji(req, level, redirect_uri);
+        res.render('result', { level: 4 })
+    })
+    .get('/GOKIGEN-level3', (req, res) => {
+        const level = 3;
+        const redirect_uri = "https://gokigen-appeal.herokuapp.com/GOKIGEN-level3";
+        set_status_emoji(req, level, redirect_uri);
+        res.render('result', { level: 3 })
+    })
+    .get('/GOKIGEN-level2', (req, res) => {
+        const level = 2;
+        const redirect_uri = "https://gokigen-appeal.herokuapp.com/GOKIGEN-level2";
+        set_status_emoji(req, level, redirect_uri);
+        res.render('result', { level: 2 })
+    })
+    .get('/GOKIGEN-level1', (req, res) => {
+        const level = 1;
+        const redirect_uri = "https://gokigen-appeal.herokuapp.com/GOKIGEN-level1";
+        set_status_emoji(req, level, redirect_uri);
+        res.render('result', { level: 1 })
     })
 
 app.listen(PORT, () => {
---
+++ set_status_emoji.js
@@ -0,0 +1,44 @@
+const request = require('request')
+const emoji_list = [
+    { text: 'めっちゃ元気', emoji: ':star-struck:' },
+    { text: 'かなり元気', emoji: ':blush:' },
+    { text: 'ふつう', emoji: ':slightly_smiling_face:' },
+    { text: '元気ない', emoji: ':expressionless:' },
+    { text: 'だめです', emoji: ':anguished:' },
+];
+
+module.exports = (req, level, redirect_uri) => {
+    const code = req.query["code"];
+    request({
+        url: "https://slack.com/api/oauth.access",
+        method: "POST",
+        form: {
+            client_id: process.env.SLACK_CLIENT_ID,
+            client_secret: process.env.SLACK_CLIENT_SECRET,
+            code: code,
+            redirect_uri: redirect_uri
+        }
+    }, (error, response, body) => {
+        const param = JSON.parse(body);
+        const access_token = param['access_token'];
+        request("https://slack.com/api/auth.test", {
+            method: "POST",
+            form: {
+                token: access_token
+            }
+        }, (error, response, body) => {
+            const user = JSON.parse(body);
+            request("https://slack.com/api/users.profile.set", {
+                method: 'POST',
+                form: {
+                    token: access_token,
+                    profile: JSON.stringify({
+                        "status_text": emoji_list[5-level].text,
+                        "status_emoji": emoji_list[5-level].emoji
+                    })
+                }
+            }, (error, response, body) => {
+            })
+        })
+    })
+}
